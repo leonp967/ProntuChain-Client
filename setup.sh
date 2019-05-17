577f598f5bdd1588fb9bd6c4f2dd0168c42ba77e
Leonardo Porto <leonardoporto159ster@gmail.com>
2019-05-17 16:58:01 -0300

    Script consertado

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 !/bin/sh
 sudo apt-get update
-sudo apt-get install curl
+sudo apt-get install curl -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 source ~/.bashrc
 nvm install 9.0.0
-sudo apt-get install python
+sudo apt-get install python -y
 npm install
 npm rebuild grpc --build-from-source --runtime=electron --target=4.2.2 --target_arch=x64 --dist-url=https://atom.io/download/electron
