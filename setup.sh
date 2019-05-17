e47737d772269b5a413b87c77713485a326a4b20
Leonardo Porto <leonardoporto159ster@gmail.com>
2019-05-17 17:54:48 -0300

    Script atualizado

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 !/bin/sh
 sudo apt-get update
-sudo apt-get install curl -y
+sudo apt-get install -y \
+    curl \
+    software-properties-common \
+    g++ \
+    python \
+    p7zip-full
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install 9.0.0
-sudo apt-get install python -y
 npm install
 npm rebuild grpc --build-from-source --runtime=electron --target=4.2.2 --target_arch=x64 --dist-url=https://atom.io/download/electron
+7z x prontuchain-keys.zip -o$HOME
