509520d134a3012a1297426b389b1214a2f20a0d
Leonardo Porto <leonardoporto159ster@gmail.com>
2019-05-17 16:55:14 -0300

    Adicionado script de setup do ambiente

---
+++ setup.sh
@@ -0,0 +1,9 @@
+!/bin/sh
+sudo apt-get update
+sudo apt-get install curl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+source ~/.bashrc
+nvm install 9.0.0
+sudo apt-get install python
+npm install
+npm rebuild grpc --build-from-source --runtime=electron --target=4.2.2 --target_arch=x64 --dist-url=https://atom.io/download/electron
